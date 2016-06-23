e1adbbb55670d2311c18da640ae19fab64dc72e2
Marco <marcoaremail@gmail.com>
2016-06-23 19:29:34 -0300

    Header to include all spatial headers

---
+++ spatial.h
@@ -0,0 +1,20 @@
+#ifndef MODULE_SPATIAL_SPATIAL_H_
+#define MODULE_SPATIAL_SPATIAL_H_
+
+#include "defaults.h"
+#include "intersection.h"
+#include "line.h"
+#include "plane.h"
+#include "quaternion.h"
+#include "type_traits.h"
+#include "vec.h"
+
+#include "shape/box.h"
+#include "shape/csg_tree.h"
+#include "shape/cylinder.h"
+#include "shape/polyhedron.h"
+#include "shape/shape.h"
+#include "shape/sphere.h"
+#include "shape/transformed_shape.h"
+
+#endif
