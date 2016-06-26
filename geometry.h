468802b5f70ee025289853319149244789e3889b
Marco <marcoaremail@gmail.com>
2016-06-26 02:19:53 -0300

    New headers

--- geometry.h
+++ geometry.h
@@ -1,9 +1,11 @@
 #ifndef MODULE_GRAPHICS_GEOMETRY_SPATIAL_H_
 #define MODULE_GRAPHICS_GEOMETRY_SPATIAL_H_
 
+#include "camera.h"
 #include "defaults.h"
 #include "intersection.h"
 #include "line.h"
+#include "parametric.h"
 #include "plane.h"
 #include "quaternion.h"
 #include "type_traits.h"
