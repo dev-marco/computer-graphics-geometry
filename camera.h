b65442f2ae797c8218ea1dbe6a9b835c8f08713d
Marco <marcoaremail@gmail.com>
2016-06-27 13:31:29 -0300

    Pre-calculate camera direction

--- camera.h
+++ camera.h
@@ -8,26 +8,27 @@
 namespace Geometry {
 
     class Camera {
-        Geometry::Vec<3> position, look_at, up_dir;
+        Geometry::Vec<3> position, look_at, up_dir, direction;
         float_max_t fov;
 
     public:
         Camera (void) {}
 
         Camera (const Vec<3> &_position, const Vec<3> &_look_at, const Vec<3> &_up_dir, const float_max_t &_fov) :
-            position(_position), look_at(_look_at), up_dir(_up_dir), fov(_fov) {}
+            position(_position), look_at(_look_at), up_dir(_up_dir), direction((_look_at - _position).normalized()), fov(_fov) {}
 
         inline const Geometry::Vec<3> &getPosition (void) const { return this->position; }
         inline const Geometry::Vec<3> &getLookAt (void) const { return this->look_at; }
         inline const Geometry::Vec<3> &getUpDirection (void) const { return this->up_dir; }
         inline const float_max_t &getFieldOfView (void) const { return this->fov; }
+        inline const Geometry::Vec<3> &getDirection (void) const { return this->direction; }
 
-        inline void setPosition (const Geometry::Vec<3> &_position) { this->position = _position; }
-        inline void lookAt (const Geometry::Vec<3> &_look_at) { this->look_at = _look_at; }
+        inline void setPosition (const Geometry::Vec<3> &_position) { this->position = _position, this->direction = (this->getLookAt() - _position).normalized(); }
+        inline void lookAt (const Geometry::Vec<3> &_look_at) { this->look_at = _look_at, this->direction = (_look_at - this->getPosition()).normalized(); }
         inline void setUpDirection (const Geometry::Vec<3> &_up_dir) { this->up_dir = _up_dir.normalized(); }
         inline void setFieldOfView (const float_max_t &_fov) { this->fov = _fov; }
 
-        inline void rotate (const Geometry::Quaternion &rot) { rot.rotate(this->look_at, this->getPosition()); }
+        inline void rotate (const Geometry::Quaternion &rot) { rot.rotate(this->look_at, this->getPosition()), this->direction = (this->look_at - this->getPosition()).normalized(); }
     };
 }
 
